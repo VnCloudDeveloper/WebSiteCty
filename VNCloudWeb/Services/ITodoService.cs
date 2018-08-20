d63fdfe2fc36416cf52b51004d29fcbfad9a0085
hungzr <36071597+hungzr@users.noreply.github.com>
2018-08-21 00:21:40 +0700

    header+ footer lỗi font

---
+++ VNCloudWeb/Services/ITodoService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VNCloudWeb.Services
+{
+    public interface ITodoService
+    {
+		Task<int> GetIncompleteItemsAsync();
+	}
+}
---
+++ VNCloudWeb/Services/TodoService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace VNCloudWeb.Services
+{
+    public class TodoService : ITodoService
+    {
+		public Task<int> GetIncompleteItemsAsync() 
+		{
+			return Task.FromResult(1);
+		}
+
+	}
+}
