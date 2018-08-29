46ae38451241ac078ebc4bdf73dc45c4a06bf6d9
hungzr <36071597+hungzr@users.noreply.github.com>
2018-08-29 09:50:29 +0700

    của luật

--- VNCloudWeb/Controllers/HomeController.cs
+++ VNCloudWeb/Controllers/HomeController.cs
@@ -17,21 +17,7 @@ namespace VNCloudWeb.Controllers
 			
 			return View();
         }
-
-        public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
-            return View();
-        }
-
-        public IActionResult Contact()
-        {
-            ViewData["Message"] = "Your contact page.";
-
-            return View();
-        }
-
+        
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
